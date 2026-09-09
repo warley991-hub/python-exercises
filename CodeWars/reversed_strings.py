88f61418c76c33f4fa2d9018d813652321d618cd
warley991-hub <brenowarley991@gmail.com>
2026-09-09 14:14:09 -0300

    feat: solve 8 kyu challenge

---
+++ CodeWars/reversed_strings.py
@@ -0,0 +1,12 @@
+'''
+Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+
+def solution(string):
+    pass
+'''
+
+def solution(string):
+    return string[::-1]
