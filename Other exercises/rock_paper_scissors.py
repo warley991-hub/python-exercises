37fd8e1b629cdc310a8e2ba27d2024888ab51974
warley991-hub <brenowarley991@gmail.com>
2026-09-14 21:06:06 -0300

    Include new Python challenge

---
+++ Other exercises/rock_paper_scissors.py
@@ -0,0 +1,24 @@
+import random
+
+choices = ["rock", "paper", "scissors"]
+
+while True:
+    user_choice = input("Enter Rock, Paper, or Scissors: ").lower()
+    
+    if user_choice == 'quit':
+        print("Thanks for playing!")
+        break
+        
+    if user_choice not in choices:
+        print("Invalid choice.")
+        continue
+        
+    computer_choice = random.choice(choices)
+    print(f"Computer chose: {computer_choice}")
+    
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
+    else:
+        print("You lose!")
