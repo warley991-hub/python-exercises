dfbe3e7b205fb5a338cc412efec39c5da67bdca1
warley991-hub <brenowarley991@gmail.com>
2026-09-21 18:54:02 -0300

    Include new Python challenges

--- Curso em Video/desafio041.py
+++ Curso em Video/desafio041.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 MIRIM = 9
 INFANTIL = 14
 JUNIOR = 19
@@ -6,5 +8,24 @@ SENIOR = 25
 def main():
     try:
         ano_nasc = int(input('Informe o ano do seu nascimento: '))
+        ano_atual = date.today().year
+        idade = ano_atual - ano_nasc
+        
+        print(f'O atleta tem {idade} anos.')
+        
+        if idade <= MIRIM:
+            print('Classificação: MIRIM')
+        elif idade <= INFANTIL:
+            print('Classificação: INFANTIL')
+        elif idade <= JUNIOR:
+            print('Classificação: JÚNIOR')
+        elif idade <= SENIOR:
+            print('Classificação: SÊNIOR')
+        else:
+            print('Classificação: MASTER')
+
     except ValueError:
-        print('Digite somente números.')
+        print('Erro: Digite somente números inteiros válidos.')
+
+if __name__ == '__main__':
+    main()
