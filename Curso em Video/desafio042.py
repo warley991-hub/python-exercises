d8d3a41fb92e31e28909dcf0d522c17f98ff7f1a
warley991-hub <brenowarley991@gmail.com>
2026-09-22 17:34:39 -0300

    Include new Python challenges

---
+++ Curso em Video/desafio042.py
@@ -0,0 +1,15 @@
+reta_a = float(input('Digite o comprimento da primeira reta: '))
+reta_b = float(input('Digite o comprimento da segunda reta: '))
+reta_c = float(input('Digite o comprimento da terceira reta: '))
+
+
+if (reta_a + reta_b > reta_c) and (reta_a + reta_c > reta_b) and (reta_b + reta_c > reta_a):
+    print('Forma um triângulo ', end='')
+    if reta_a == reta_b == reta_c:
+        print('Equilátero.')
+    elif reta_a != reta_b != reta_c != reta_a:
+        print('Escaleno.')
+    else:
+        print('Isósceels.')
+else:
+    print('Não forma um triângulo ', end='')
