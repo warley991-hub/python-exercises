b4decc61799160898c4b1180ae5883c678699d7c
warley991-hub <brenowarley991@gmail.com>
2026-09-22 17:23:37 -0300

    feat: solve 8 kyu challenge

---
+++ CodeWars/generate_range_of_integers.py
@@ -0,0 +1,18 @@
+'''
+Description:
+Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) in increments of step.
+
+Examples
+(1, 10, 1) -> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+(-10, 1, 1) -> [-10, -9, -8, -7, -6, -5, -4, -3, -2, -1, 0, 1]
+(1, 15, 20) -> [1]
+Note
+start < stop
+step > 0
+
+def generate_range(start, stop, step):
+    pass
+'''
+
+def generate_range(start, stop, step):
+    return [i for i in range(start,stop+1,step)]
